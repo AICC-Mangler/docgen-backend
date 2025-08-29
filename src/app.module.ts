5d02a4c9e7984eef1397a6e3b340421281d60039
김준영 <1090jun@naver.com>
2025-08-29 14:18:32 +0900

    app.modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MemberModule, ProjectModule, TimelineModule } from './modules/module';
+import {
+  MemberModule,
+  ProjectModule,
+  TimelineModule,
+  DocumentModule,
+} from './modules/module';
 import { AuthenticationModule } from './modules/authentication.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { JWT_MODULE_OPTIONS } from './config/jwt.config';
-
+import { HttpModule } from '@nestjs/axios';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
