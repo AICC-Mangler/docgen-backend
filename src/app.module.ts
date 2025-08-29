0dbe3485a536edf25bc540dad74be4b57e250b11
ohminju <rjatnsdl5755@naver.com>
2025-08-29 12:26:32 +0900

    mod app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './modules/module';
+import { AuthenticationModule } from './modules/authentication.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { JWT_MODULE_OPTIONS } from './config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), MemberModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    JwtModule.register(JWT_MODULE_OPTIONS),
+    MemberModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
